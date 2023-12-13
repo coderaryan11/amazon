53bcfd345d2e2f3d8019624f6866f4ac79cf8405
coderaryan11 <120705086+coderaryan11@users.noreply.github.com>
2023-12-13 23:14:40 +0530

    Amazon clone testing

---
+++ Amazon/amazon.js
@@ -0,0 +1,16 @@
+// const searchInput = document.querySelector('.search-input');
+// const suggestions = document.querySelector('.suggestions');
+
+// // Show suggestions on input click
+// searchInput.addEventListener('click', () => {
+//     suggestions.style.display = 'block';
+// });
+
+// // Handle suggestion selection
+// suggestions.addEventListener('click', (event) => {
+//     const selectedSuggestion = event.target;
+//     if (selectedSuggestion.tagName === 'LI') {
+//         searchInput.value = selectedSuggestion.textContent;
+//         suggestions.style.display = 'none';
+//     }
+// });
